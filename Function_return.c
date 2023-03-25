3c23a1239d5aa810e48899a02e24628177bd9f6f
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:50:10 +0530

    Create Function_return.c

---
+++ Function_return.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+float sum (float,float);
+int c;
+void main()
+{
+    float a=2.360,b=6.6873;
+    float total=sum(a,b);
+}
+float sum(float n1,float n2)
+{
+    return n1+n2;
+}
