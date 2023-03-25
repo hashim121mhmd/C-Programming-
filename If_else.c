6c8d7babfd57d62bc41cf9412c5dbc3a2410d99c
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:16:04 +0530

    Create If_else.c

---
+++ If_else.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a=123; //initializing
+printf("Enter a Number\n");
+scanf("%d",&a);
+if(a%2==0)
+{
+    printf("Number %d is a even Number",a);
+}
+else
+{
+    printf("Number %d is a odd Number",a);
+}
+}
+/* conditional statements
+1. if else..if
+
+syntax
+if(condition)
+{
+    code;
+}
+else
+{
+
+}
+*/
