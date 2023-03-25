945ca510a5431baab3fbdaf0aabadf41cf1e2efa
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:38:30 +0530

    Create Array_intro.c

---
+++ Array_intro.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[5];//char array declaration
+int z;
+char b[5]={1,56,23,86,z};//declaration and init
+char c[]={1,2,3,4,5,6,7};//c[7]={1,2,3,4,5,6,7}
+char d[5]={1,2};//
+int f[]={10,20,30,40,50};
+int g[5];
+int h[5]={10,20,30,40,50};
+int i[5]={100,200};
+printf("b[0]=%d\nb=%d",&b[0],b);
+}
