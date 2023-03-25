557770fcd323a5c5064b630f859e6b7e71ddacc4
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:43:59 +0530

    Create Function_3.c

---
+++ Function_3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void sum(void);
+int num=234,num1=456;
+int num=234,num1=456;
+void main()
+{
+
+   /* printf("Enter first Number : ");
+    scanf("%d",&num);
+    printf("Enter Second Number : ");
+    scanf("%d",&num1);*/
+    sum();
+    printf("\n num is %d",num);
+     printf("\n num1 is %d",num1);
+    getch();
+}
+
+void sum(void)
+{
+    int num,num1;
+    int num,num1;
+    num=156;
+    num1=135;
+    printf("\n a= %d",num);
+    printf("\n b= %d",num1);
+}
