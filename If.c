ca5c20c2832db81859b9c436419cb80c948ca73d
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:15:10 +0530

    Create If.c

---
+++ If.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a=123; //initializing
+printf("Enter a Number\n");
+scanf("%d",&a);
+if(a%2==0)
+{
+    printf("Number %d is even",a);
+}
+ if(a%2==1)
+{
+    printf("Number %d is odd",a);
+}
+
+}
+/* conditional statements
+1. if
+
+syntax
+if(condition)
+{
+    code;
+}
+
+*/
