0e12bd5d8a147222490cf3b029725b07dc8f37ee
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:44:28 +0530

    Create Local_global.c

---
+++ Local_global.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int a;
+int b;
+void main()
+{
+    int d;
+
+   for(int i=0;i<=11;i++)
+   {
+       int j;
+       int i1;
+       if(i%2==0)
+       {
+           int k=120;
+           a++;
+       }
+
+       else
+        {
+            int q12=360;
+        }
+
+   }
+
+
+
+}
