c2d9668a5e002c38148f9e421447f60b5ac31a41
Muhammed Hashim k <111489525+hashim121mhmd@users.noreply.github.com>
2023-03-25 08:19:44 +0530

    Create Switch_case_default.c

---
+++ Switch_case_default.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int a=123; //initializing
+printf("Enter a Number\n");
+scanf("%d",&a);
+switch(a%3)
+{
+case 0:
+    printf("Number %d is divisible by 3",a);
+break;
+case 1:
+    printf("Number %d is not divisible rem=1",a);
+break;
+default:
+    printf("Number %d is not divisible rem=2",a);
+    break;
+}
+}
+/* conditional statements
+1. switch..case
+
+syntax
+switch(condition)
+{
+    case 0:
+    stmt;
+    break;
+    case 1:
+    stmt;
+    break;
+    case 2:
+    stmt;
+    break;
+    ----
+    default:
+    stmts;
+    break
+}
+*/
